f5ab5fde9ccc1fd1860afd0f8e07bac5f208c56e
mabubakar365 <m.abubakar365@yahoo.com>
2024-03-20 20:14:42 +0500

    Updated finder-test.sh

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -9,7 +9,7 @@ NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 # username=$(cat conf/username.txt)
-username=$(cat etc/finder-app/conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
